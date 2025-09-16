7bf52489b9bff09151408f37c94d55f42c6b7835
Vander <vandercaneppelle@outlook.com>
2025-09-16 00:00:56 -0300

    notificacao da noite funcionando

--- cript send-notification.js
+++
@@ -1,3 +0,0 @@
-[33ma39a0cb[m[33m ([m[1;36mHEAD -> [m[1;32madd-push-notifications[m[33m, [m[1;31morigin/add-push-notifications[m[33m)[m  changed time test
-[33m329e84f[m feat: separar workflows em arquivos individuais
-[33m44e1a06[m refactor: simplificar controle de horários das notificações
